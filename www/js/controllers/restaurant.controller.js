0801c5d3d1573e8273cb89b4228aa1e085dc8b67
kieboom-aaron <aaronkieboom@gmail.com>
2015-03-24 14:50:53 +0100

    Update current location

--- www/js/controllers/restaurant.controller.js
+++ www/js/controllers/restaurant.controller.js
@@ -5,13 +5,18 @@ angular.module('diningapp').controller('restaurantController', ['$scope', 'eetNu
         $ionicLoading.show({
             template: 'Loading...'
         });
-        eetNu.getRestaurants(function(data) {
-            $scope.restaurants = data;
-            $ionicLoading.hide();
-        }, {
-            lat: 51.52793,
-            lng: 5.081889
-        });
+        navigator.geolocation.getCurrentPosition(function(data){
+            console.log(data);
+            eetNu.getRestaurants(function(data) {
+                $scope.restaurants = data;
+                $ionicLoading.hide();
+            }, 
+            {
+                lat : data.coords.latitude,
+                lng : data.coords.longitude
+            });
+        })
+        
 
         $scope.doRefresh = function() {
             eetNu.getRestaurants(function(data) {

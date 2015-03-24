162cac528a4b5ebebce59b55f2babaed617e9391
Michael van de Ven <michael.vd.ven95@gmail.com>
2015-03-24 15:23:53 +0100

    changes

---
+++ www/js/controllers/restaurant.controller.js
@@ -0,0 +1,30 @@
+angular.module('diningapp').controller('restaurantController', ['$scope', 'eetNu', '$ionicLoading',
+    function($scope, eetNu, $ionicLoading) {
+
+        //open loading dialog
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+        eetNu.getRestaurants(function(data) {
+            $scope.restaurants = data;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+            $ionicLoading.hide();
+        });
+
+
+        $scope.doRefresh = function() {
+            navigator.geolocation.getCurrentPosition(function(data) {
+                eetNu.getRestaurants(function(data) {
+                    $scope.restaurants = data;
+                    $scope.$broadcast('scroll.refreshComplete');
+
+                }, {
+                    lat: data.coords.latitude,
+                    lng: data.coords.longitude
+                });
+            });
+        };
+    }
+]);
